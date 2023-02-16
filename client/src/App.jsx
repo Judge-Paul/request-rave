0d667e6b8b1163de5bfd61a1859cbd66d862b72b
unknown <judgepaulogebe3@gmail.com>
2023-02-16 16:56:15 +0100

    feat: change design and make responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,21 @@ import UserIcon from "./assets/UserIcon";
 
 export default function App() {
   return  (
-    <div className="md:flex md:items-center justify-center h-screen">
-      <div className="w-full md:w-[700px] text-center">
-        <p className="text-5xl font-bold">
+    <div className="w-full px-8 md:px-24 lg:px-48 xl:px-80 pt-24 mx-auto pb-14">
+      <div className="text-center">
+        <p className="text-4xl md:text-5xl font-bold text-blue-900">
           Which are you?
         </p>
-        <div className="mt-24 md:flex">
+        <div className="mt-10 px-5 grid md:grid-cols-2 gap-10 place-items-center">
           <button
-            className="p-0 md:mr-10 rounded-xl bg-black hover:bg-white hover:border hover:border-black"
+            className="p-0 rounded-xl bg-white hover:bg-blue-900 border border-blue-900 w-full"
           >
-            <UserIcon className="fill-current text-white hover:text-black p-5" />
+            <UserIcon className="fill-current text-blue-900 hover:text-white p-5" />
           </button>
           <button
-            className="p-0 md:ml-10 rounded-xl bg-black hover:bg-white hover:border hover:border-black"
+            className="p-0 rounded-xl bg-white hover:bg-blue-900 border border-blue-900 w-full"
           >
-            <DjIcon className="fill-current text-white hover:text-black p-5" />
+            <DjIcon className="fill-current text-blue-900 hover:text-white p-5" />
           </button>
         </div>
       </div>
