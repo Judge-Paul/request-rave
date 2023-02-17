8797666ad006cdf6a95eda6f6b2b3803ebb196a1
unknown <judgepaulogebe3@gmail.com>
2023-02-17 02:16:05 +0100

    feat: create song card and display on dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
 
 export default function App() {
   return  (
-    <HomePage />
+    // <HomePage />
+    <DashboardPage />
   )
 }
