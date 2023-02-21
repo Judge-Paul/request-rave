1ee895c27335ac1c41b86df2005941a4f5fd68ff
unknown <judgepaulogebe3@gmail.com>
2023-02-21 23:50:55 +0100

    feat: make api call to get access token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,37 @@ import DashboardPage from "./pages/DashboardPage";
 import RequestPage from "./pages/RequestPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import io from "socket.io-client"
+import io from "socket.io-client";
+import axios from 'axios';
 
 export default function App() {
-  const socket = io.connect(`${import.meta.env.VITE_REACT_APP_SERVER_URL}`);
+  const CLIENT_ID=`${import.meta.env.VITE_CLIENT_ID}`
+  const CLIENT_SECRET=`${import.meta.env.VITE_CLIENT_SECRET}`
+  const [accessToken, setAccessToken] = useState("")
+  const socket = io.connect(`${import.meta.env.VITE_REACT_APP_SERVER_URL}`)
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      const authParameters = {
+        grant_type: 'client_credentials',
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET
+      };
+      try {
+        const response = await axios.post('https://accounts.spotify.com/api/token', authParameters, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        });
+        setAccessToken(response.data.access_token);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchAccessToken();
+  }, []);
+  
   return  (
     <BrowserRouter>
       <NavBar />
