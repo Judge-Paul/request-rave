0506532495a2c888a7df230e864604706177d7a7
unknown <judgepaulogebe3@gmail.com>
2023-02-24 11:08:57 +0100

    fix: display more than one song on the dasboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,47 +3,57 @@ import Song from "./Song";
 import axios from "axios";
 
 export default function Dashboard({ socket, accessToken }) {
-    const [played, setPlayed] = useState(true);
-    const [songs, setSongs] = useState([]);
-    const [trackIds, setTrackIds] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [trackIds, setTrackIds] = useState([]);
 
-    useEffect(() => {
-        socket.on("receive-message", (trackId) => {
-            setTrackIds((trackIds) => [...trackIds, trackId]);
-        });
-    
-        return () => {
-            socket.off("message");
-        };
-    }, [socket]);
+  useEffect(() => {
+    socket.on("receive-message", (trackId) => {
+      setTrackIds((trackIds) => [...trackIds, trackId]);
+    });
 
-    useEffect(() => {
-        trackIds.map((trackId) => {
-            let url = `https://api.spotify.com/v1/tracks/${trackId}`
-            axios.get(url, {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-                })
-                .then(response => {
-                console.log(response.data);
-                setSongs(displaySongs(response.data))
-                })
-                .catch(error => {
-                console.log(error);
-                });
+    return () => {
+      socket.off("receive-message");
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    const requests = trackIds.map((trackId) => {
+      let url = `https://api.spotify.com/v1/tracks/${trackId}`;
+      axios
+        .get(url, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         })
-    }, [trackIds]) 
+        .then((response) => {
+          console.log(response.data);
+          const songData = response.data;
+          const artist = Array.isArray(songData.artists)
+            ? songData.artists.map((a) => a.name).join(", ")
+            : songData.artists && songData.artists.name;
+          const song = (
+            <Song
+              key={songData.id}
+              albumCover={songData.album.images[2].url}
+              title={songData.name}
+              artist={artist}
+              album={songData.album.name}
+              time="3:32"
+            />
+          );
+          setSongs((prev) => [...prev, song]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  }, [trackIds, accessToken]);
 
-    function displaySongs({name, artists, album }) {
-        const artist = Array.isArray(artists) ? artists.map(a => a.name).join(", ") : (artists && artists.name)
-        return <Song albumCover={album.images[2].url} title={name} artist={artist} album={album.name} time="3:32" />
-    }
-    return (
-        <div className="min-h-screen pt-20 px-36 pb-14">
-            <h1>Dashboard Page</h1>
-            {songs}
-            {/* <ul>
+  return (
+    <div className="min-h-screen pt-20 px-36 pb-14">
+      <h1>Dashboard Page</h1>
+      {songs}
+      {/* <ul>
                 {messages.map((message, index) => (
                     <li key={index}>{message}</li>
                 ))}
@@ -59,6 +69,6 @@ export default function Dashboard({ socket, accessToken }) {
             </p>
             <Song title="Rush" artist="Arya Starr" album="19 & Dangerous" time="4:09" isPlayable={played} />
             <Song title="Rapstar" artist="Polo G" album="Hall of Fame" time="3:32" isPlayable={played} /> */}
-        </div>
-    )
+    </div>
+  );
 }
