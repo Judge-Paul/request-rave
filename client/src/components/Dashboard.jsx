ff01506e70fbdbcd2fb40ac6f53ca61e88e3d457
unknown <judgepaulogebe3@gmail.com>
2023-02-24 17:01:00 +0100

    fix: stop songs from showing more than once

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -31,17 +31,27 @@ export default function Dashboard({ socket, accessToken }) {
           const artist = Array.isArray(songData.artists)
             ? songData.artists.map((a) => a.name).join(", ")
             : songData.artists && songData.artists.name;
-          const song = (
-            <Song
-              key={songData.id}
-              albumCover={songData.album.images[2].url}
-              title={songData.name}
-              artist={artist}
-              album={songData.album.name}
-              time="3:32"
-            />
-          );
-          setSongs((prev) => [...prev, song]);
+            setSongs(prev => {
+                const newSong = (
+                  <Song
+                    key={songData.id}
+                    albumCover={songData.album.images[2].url}
+                    title={songData.name}
+                    artist={artist}
+                    album={songData.album.name}
+                    time="3:32"
+                  />
+                );
+                if (prev.find(song => song.key === newSong.key)) {
+                  // Song already exists in the array, return previous array
+                  return prev;
+                } else {
+                  // Add new song to the array
+                  return [...prev, newSong];
+                }
+              });
+              
+          console.log(songs)
         })
         .catch((error) => {
           console.log(error);
@@ -51,24 +61,13 @@ export default function Dashboard({ socket, accessToken }) {
 
   return (
     <div className="min-h-screen pt-20 px-36 pb-14">
-      <h1>Dashboard Page</h1>
+      <p className="text-4xl md:text-5xl font-bold text-blue-900 text-center">
+        Requests
+      </p>
       {songs}
-      {/* <ul>
-                {messages.map((message, index) => (
-                    <li key={index}>{message}</li>
-                ))}
-            </ul> */}
-            {/* <p className="text-4xl md:text-5xl font-bold text-blue-900 text-center">
-                Requests
-            </p>
-            <Song title="Rush" artist="Arya Starr" album="19 & Dangerous" time="4:09" />
-            <Song title="Rapstar" artist="Polo G" album="Hall of Fame" time="3:32" />
-            <Song title="Moonlight Sonata" artist="Ludwig Van Beethoven" album="" time="7:49" />
-            <p className="pt-20 text-3xl md:text-4xl font-bold text-blue-900 text-center">
-                Recently Played
-            </p>
-            <Song title="Rush" artist="Arya Starr" album="19 & Dangerous" time="4:09" isPlayable={played} />
-            <Song title="Rapstar" artist="Polo G" album="Hall of Fame" time="3:32" isPlayable={played} /> */}
+      <p className="pt-20 text-3xl md:text-4xl font-bold text-blue-900 text-center">
+        Recently Played
+      </p>
     </div>
   );
 }
